3360bfb1f65b1c272e18b5cff867a53f09c5104a
Reema <it.reema7@gmail.com>
2019-05-18 22:28:20 +0300

    Add files via upload

---
+++ Whack-a-Penguin/Whack-a-Penguin/WhackSlot.swift
@@ -0,0 +1,34 @@
+//
+//  WhackSlot.swift
+//  Whack-a-Penguin
+//
+//  Created by mac_os on 13/09/1440 AH.
+//  Copyright © 1440 mac_os. All rights reserved.
+//
+
+import UIKit
+import SpriteKit
+
+class WhackSlot: SKNode {
+    var charNode: SKSpriteNode!
+    
+    func configure(at position: CGPoint) {
+        self.position = position
+        
+        let sprite = SKSpriteNode(imageNamed: "whackHole")
+        addChild(sprite)
+        
+        let cropNode = SKCropNode()
+        cropNode.position = CGPoint(x: 0, y: 15)
+        cropNode.zPosition = 1
+        cropNode.maskNode = SKSpriteNode(imageNamed: "whackMask")
+        
+        charNode = SKSpriteNode(imageNamed: "penguinGood")
+        charNode.position = CGPoint(x: 0, y: -90)
+        charNode.name = "character"
+        cropNode.addChild(charNode)
+        
+        addChild(cropNode)
+    }
+
+}
